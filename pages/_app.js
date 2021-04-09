989b88a1ae3af74cfa8d1af00773557bd2d2ffe4
Kevin Adams <kadams34@cnm.edu>
2021-04-09 09:15:51 -0600

    playing around with bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Layout from '../components/layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,42 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
+
+  function getCurrentYear() {
+    return new Date().getFullYear()
+}
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+      <h1 className="font-weight-bold">Kevin Adams</h1>
+      <h1 className="display-2 font-italic">Full Stack web developer</h1>
+      <h1 className="display-3 text-strong"><Link href='/posts/first-post'>Albuquerque, NM</Link></h1>
 
-        <p className={styles.description}>
+        <p>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div>
+          <a href="https://nextjs.org/docs">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn">
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -40,7 +44,6 @@ export default function Home() {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
             <p>
@@ -50,15 +53,8 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer className="bg-dark navbar text-white">
+        <p>©<a className="text-white text-decoration-none" href="https://github.com/kadams34">Kevin Adams</a>&nbsp;|&nbsp;{getCurrentYear()}</p>
       </footer>
     </div>
   )
