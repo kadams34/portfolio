006128abc0992d698cc03be6d19e05387fe9f07a
Kevin Adams <kadams34@cnm.edu>
2021-06-10 15:38:10 -0600

    added formik and yup

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,120 +1,114 @@
 import { Form } from 'react-bootstrap';
 import emailjs from 'emailjs-com'
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 
 
-class ContactForm extends React.Component {
-    
-    constructor(props) {
-        super(props)
+export default function ContactForm() {
 
-        this.state = {
-            userName: '',
-            userEmail: '',
-            subject: '',
-            message: ''
-        }
-
-        this.handleNameChange = this.handleNameChange.bind(this)
-        this.handleEmailChange = this.handleEmailChange.bind(this)
-        this.handleSubjectChange = this.handleSubjectChange.bind(this)
-        this.handleMessageChange = this.handleMessageChange.bind(this)
-
-        
+    const contactValues = { 
+        user_name:"", 
+        user_email:"", 
+        subject:"", 
+        message:"" 
     }
 
-    handleNameChange = (e) => {
-        this.setState({
-            userName: e.target.value
-        })
-    }
+    const validation = Yup.object().shape({
+        user_name: Yup.string()
+            .min(2, "I feel like one word names are for the super elite. Nice try, Elon.")
+            .max(100, "Your name is not that long, is it?")
+            .required("C'mon, you have a name"),
+        user_email: Yup.string()
+            .email("That's not an email you silly goose")
+            .max(100, "That's a really long email there, sport")
+            .required("just make one up"),
+        subject: Yup.string(),
+        message: Yup.string()
+            .min(5, "Elaborate please")
+            .max(200, "Whoa whoa whoa too much!")
+            .required("please tell me a story")
+      })
 
-    handleEmailChange = (e) => {
-        this.setState({
-            userEmail: e.target.value
-        })
-    }
+    function sendEmail(e) {
+        e.preventDefault();
 
-    handleSubjectChange = (e) => {
-        this.setState({
-            subject: e.target.value
-        })
-    }
-
-    handleMessageChange = (e) => {
-        this.setState({
-            message: e.target.value
-        })
+        emailjs.sendForm('adamke_contact', 'contact_form', e.target, 'user_7moDwUlcYDRp8dkut2iTr')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
     }
     
+
     
-
-    // const [validated, setValidated] = useState(false);
-
-    // validateForm(event) {
-    //     const form = event.currentTarget;
-    //     if (form.checkValidity() === false) {
-    //         event.preventDefault();
-    //         event.stopPropagation();
-    //     }
-
-    //     setValidated(true);
-    // };
-
-    handleSubmit = (e) => {
-        e.preventDefault()
-        alert(`Email sent successfully!`)
-        // validateForm(e)
-        
-        this.setState({
-            userName: '',
-            userEmail: '',
-            subject: '',
-            message: ''
-        })
-
-        this.sendEmail = (e) => {
-            e.preventDefault();
-    
-            emailjs.sendForm('adamke_contact', 'contact_form', e.target, 'user_7moDwUlcYDRp8dkut2iTr')
-                .then((result) => {
-                    console.log(result.text);
-                }, (error) => {
-                    console.log(error.text);
-                });
-        }
-    }
-
-    render() {
-        return (
-            <Form className="contact-form" onSubmit={this.handleSubmit}>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Name" name="user_name" onChange={this.handleNameChange} value={this.state.userName} required />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" name="user_email" onChange={this.handleEmailChange} value={this.state.userEmail} required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid email.
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label>Subject</Form.Label>
-                    <Form.Control type="text" placeholder="Subject" name="subject" onChange={this.handleSubjectChange} value={this.state.subject} required />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Message</Form.Label>
-                    <Form.Control as="textarea" rows={3} name="message" onChange={this.handleMessageChange} value={this.state.message} required />
-                </Form.Group>
-                <div className="text-center">
-                <button type="submit" value="Send" className="btn btn-primary btn-lg m-3">Send</button>
-                <button type="reset" className="btn btn-secondary btn-lg m-3">Reset</button>
-                </div>
-            </Form>
-        )
-    }
+    return (
+        <Formik 
+            initialValues={contactValues}
+            validationSchema={validation}
+        >
+            {( {values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting }) => (
+                    <Form className="contact-form" onSubmit={sendEmail}>
+                        <Form.Group className="pt-2" controlId="ContactFormName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                placeholder="Name" 
+                                name="user_name"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.user_name}
+                            />
+                        </Form.Group>
+                        <Form.Group className="pt-2" controlId="ContactFormEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control 
+                                type="email" 
+                                placeholder="name@example.com" 
+                                name="user_email" 
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.user_email}
+                            />
+                        </Form.Group>
+                        <Form.Group className="pt-2" controlId="ContactFormSubject">
+                            <Form.Label>Subject</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                placeholder="Subject" 
+                                name="subject" 
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.subject}
+                            />
+                        </Form.Group>
+                        <Form.Group className="pt-2" controlId="ContactFormMessage">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control 
+                                as="textarea" 
+                                rows={3} 
+                                name="message" 
+                                placeholder="Write your message here"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.message}
+                            />
+                        </Form.Group>
+                        <div className="text-center">
+                        <button type="submit" value="Send" className="btn btn-primary btn-lg m-3">Send</button>
+                        {/* <button type="reset" className="btn btn-secondary btn-lg m-3">Reset</button> */}
+                        </div>
+                    </Form>
+                )
+            }
+        </Formik>
+    )
 }
-
-export default ContactForm
