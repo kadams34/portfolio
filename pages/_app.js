2601a15d4a603e3855579c4afb5b86981e728bf6
Kevin Adams <kadams34@cnm.edu>
2021-06-10 22:17:18 -0600

    added duration time to framer motion

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps, router }) {
           <link rel="icon" href="images/doge.png" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
-        <motion.div key={router.route} exit={{ backgroundColor: 'white', filter: `invert()`, opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1  }}>
+        <motion.div key={router.route} exit={{ backgroundColor: 'white', filter: `invert()`, opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1  }} transition={{ duration: .4 }}>
           <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
